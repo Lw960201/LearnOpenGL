550235e5da51a8f61fc5fd1ec88107cf6b1c67bb
一只灬菜鸟 <478385373@qq.com>
2019-03-14 22:55:51 +0800

    渲染图片

--- OpenGLExcercise01/main.cpp
+++ OpenGLExcercise01/main.cpp
@@ -4,43 +4,24 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"//系统自带的头文件用<>，项目下的头文件用""
+//====对图片操作的api============
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 //模型顶点数据=======================================================================
 float vertices[] = {
-	 -1, -1, 0.0f,1,0,0,		//0
-	 -0.5f, 0.5f, 0.0f,0,1,0,	//1
-	 0.5f,  -0.5f, 0.0f,0,0,1,//2
-	 //-0.5f, 0.5f, 0.0f,
-	 //0.5f,  -0.5f, 0.0f,
-     1, 1, 0.0f,0.2f,0.3f,0.7f			//3
-
-	////左下角
-	//-1,-1,0, 1,0,0,//0
-	//0,-1,0,0, 1,0,//1
-	//-1,0,0, 0,0,1,//2
-	////右下角
-	//0,-1,0,0,0,1,
-	//1,-1,0, 1,0,0,//3
-	//1,0,0, 0,1,0,//4
-	////右上角
-	//1,0,0, 0,0,1,//5
-	//1,1,0, 1,0,0,//6
-	//0,1,0, 0,1,0,//7
-	////左上角
-	//0,1,0,0,0,1,
-	//-1,1,0, 1,0,0,//8
-	//-1,0,0,0,1,0
+	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
 
 };
 //用索引表示一个三角面：0，1，2    2，1，3.可以减少相同顶点的输入
 
 unsigned int indices[] = {//注意索引从0开始
-	0,1,2,//第一个三角形
-	1,2,3 //第二个三角形
-	//0,1,2,
-	//1,3,4,
-	//5,6,7,
-	//7,8,2
+	0,1,3,//第一个三角形
+	2,3,1 //第二个三角形
 };
 
 //按下ESC关闭窗口====================================================================
@@ -51,26 +32,8 @@ void processInput(GLFWwindow* window) {
 	}
 }
 
-//======================顶点着色器GLSL==============================================
-//const char* vertexShaderSource =
-//"#version 330 core										 \n"
-//"layout (location = 0) in vec3 aPos;                     \n"
-//"layout (location = 1) in vec3 aColor;                     \n"
-//"out vec4 vertexColor;                                   \n"
-//"void main(){                                            \n"
-//"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);    \n"
-//"    vertexColor = vec4(aColor.x,aColor.y,aColor.z,1.0);                    \n"
-//"}   \n";
-//=======================片段着色器GLSL=============================================
-//const char* fragmentShaderSource =
-//"#version 330 core								 \n"
-//"out vec4 FragColor;							 \n"
-//"in  vec4 vertexColor;							 \n"
-//"uniform vec4 ourColor;							 \n"
-//"void main(){                                    \n"
-//"    FragColor = vertexColor;                 }  \n";
 
-int main2()
+int main()
 {
 	//===================初始化====================================================
 	//初始化glfw库
@@ -109,12 +72,6 @@ int main2()
 	}
 
 	glViewport(0,0,800,600);
-	//框线模式
-	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-	//====================背面剔除===========================================
-	//glEnable(GL_CULL_FACE);
-	//glCullFace(GL_BACK);//opengl规定逆时针绘制三角形的正面
-	//=====================VAO===============================================
 
 	//Shader testShader = Shader("vertexSource.txt","fragmentSource.txt");
 	//testShader.test();//对象配合.使用
@@ -142,44 +99,41 @@ int main2()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 	//往GL_ELEMENT_ARRAY_BUFFER中传入数据
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+	//====================TEXTURE_2D_BUFFER============================================
+	//生成一个texBuffer
+	unsigned int texBuffer;
+	glGenTextures(1,&texBuffer);
+	//绑定，让之后的纹理命令都可以配置当前绑定的纹理
+	glBindTexture(GL_TEXTURE_2D,texBuffer);
 
-	////===================顶点着色器============================================
-	////顶点着色器操作
-	//unsigned int vertexShader;
-	////创建一个VertexShader
-	//vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	////将源文件赋值给VertexShader
-	//glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
-	////编译VertexShader
-	//glCompileShader(vertexShader);
-	////====================片段着色器===========================================
-	////片段着色器操作
-	//unsigned int fragmentShader;
-	////创建一个fragmentShader
-	//fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	////将源文件赋值给fragmentShader
-	//glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	////编译fragmentShader
-	//glCompileShader(fragmentShader);
-	////====================着色程序=============================================
-	////把shader贴到shaderProgram
-	//unsigned int shaderProgram;
-	////生成一个shaderProgrma
-	//shaderProgram = glCreateProgram();
-	////将VertexShader和FragmentShader贴到ShaderProgram
-	//glAttachShader(shaderProgram,vertexShader);
-	//glAttachShader(shaderProgram,fragmentShader);
-	////链接到ShaderProgram
-	//glLinkProgram(shaderProgram);
-
+	//放入原图
+	int width, height, nrChannels;//图宽，图高，颜色通道
+	unsigned char *data = stbi_load("container.jpg",&width,&height,&nrChannels,0);
+	if (data)
+	{
+		//
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0/*是否要边缘*/,GL_RGB,GL_UNSIGNED_BYTE,data);
+		//绑定在GL_TEXTURE_2D上的图，给他生成mipmap，必须要
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Load Image Failed.");
+	}
+	//用完内存后释放内存
+	stbi_image_free(data);
+	//================配置顶点特征值：如顶点位置，顶点颜色，纹理坐标==============================================
 	//顶点特征值，参数1：从哪里开始，参数2：一次塞多少，参数3：都是什么类型的顶点，
 	//参数4：是否正规化到±1之间，参数5：每次获取顶点间隔多少，参数6：第一次获取从偏移多少的地方开始
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6 * sizeof(float),(void*)0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8 * sizeof(float),(void*)0);
 	//开启顶点特征值，用到shader中
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
 	//开启顶点特征值，用到shader中
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	//开启顶点特征值，用到shader中
+	glEnableVertexAttribArray(2);
 
 	//===================渲染循环==================================================
 	//如果窗口没有关闭
@@ -190,19 +144,10 @@ int main2()
 		glClearColor(0.0f,0.0f,0.0f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBindTexture(GL_TEXTURE_2D,texBuffer);
 		glBindVertexArray(VAO);
 		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 
-		////获取时间
-		//float timeValue = glfwGetTime();
-		////让timeColor的值映射到0到1之间
-		//float changeTimeValue = (sin(5*timeValue) / 2.0f) + 0.5f;
-		////获取shaderProgram中的ourColor
-		//int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
-		////使用程序
-		//glUseProgram(shaderProgram);
-		////给ourColor赋值
-		//glUniform4f(vertexColorLocation, (sin(5 * timeValue) / 2.0f) + 0.5f, (sin(5 * timeValue + 0.5f) / 2.0f) + 0.5f, (sin(5 * timeValue + 1.0f) / 2.0f) + 0.5f, 1.0f);
 	
 		myShader->use();
 
